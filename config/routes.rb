4c13f9976f214dcdd2eeee0bf22dd7b3c4fcf439
tsaoyuan <tsaoyuan@gmail.com>
2021-11-20 15:18:14 +0800

    add get:'/hello' 建立hello Swinecamp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# 門口的阿桑, 問路都找阿桑
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# 建立一個hello頁面
+# 在網址列輸入/hello
+# 出現 Routing Error: uninitialized constant HelloController Did you mean? HellosController
+# 在controllers底下建HelloController.rb
+get "/hello", to: "hello#say"
 end
+
